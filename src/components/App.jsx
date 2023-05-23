513a32d8f02fa0dd142a7ddc9495d50235938b8e
Roman Broshnitskiy <romario777v8@gmail.com>
2023-05-23 07:53:03 +0300

    create project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,5 @@
+import styles from './App.module.scss';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <div className={styles.app}>React homework template</div>;
 };
